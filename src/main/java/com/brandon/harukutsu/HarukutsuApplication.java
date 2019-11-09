42d1982cc4e1705a3dff105eea602cb8b341566f
Brandon Connes <connesbl@gmail.com>
2019-11-09 18:28:37 -0500

    Harukutsu intial commit - created a RESTful API using Spring Boot capable of GET/POST/PUT/DELETE with Postgresql

---
+++ src/main/java/com/brandon/harukutsu/HarukutsuApplication.java
@@ -0,0 +1,13 @@
+package com.brandon.harukutsu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarukutsuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarukutsuApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/brandon/harukutsu/datasource/PostgresDatasource.java
@@ -0,0 +1,26 @@
+package com.brandon.harukutsu.datasource;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author bconnes
+ * @since 11/9/2019
+ */
+
+@Configuration
+public class PostgresDatasource
+{
+    @Bean
+    @ConfigurationProperties("app.datasource")
+    public HikariDataSource hikariDataSource()
+    {
+        return DataSourceBuilder
+            .create()
+            .type(HikariDataSource.class)
+            .build();
+    }
+}
